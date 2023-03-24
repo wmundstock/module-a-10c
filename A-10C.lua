afb752afa8ed5a90933107438d7e1a63d4f8f8f9
wmundstock <walter.munds@gmail.com>
2023-03-24 15:36:40 -0300

    Update A-10C.lua

--- A-10C.lua
+++ A-10C.lua
@@ -2,6 +2,7 @@ BIOS.protocol.beginModule("A-10C", 0x1000)
 BIOS.protocol.setExportModuleAircrafts({"A-10C", "A-10C_2"})
 --overhaul by WarLord
 
+
 local inputProcessors = moduleBeingDefined.inputProcessors
 local documentation = moduleBeingDefined.documentation
 
