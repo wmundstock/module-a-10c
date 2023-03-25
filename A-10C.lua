1eae9bac6d57e4502b1b58147707024715630410
wmundstock <walter.munds@gmail.com>
2023-03-25 11:42:45 -0300

    Added pinky switch
    Added pinky

--- A-10C.lua
+++ A-10C.lua
@@ -1,7 +1,7 @@
 BIOS.protocol.beginModule("A-10C", 0x1000)
 BIOS.protocol.setExportModuleAircrafts({"A-10C", "A-10C_2"})
 --overhaul by WarLord
-
+-- Updating to new version of A-10C
 
 local inputProcessors = moduleBeingDefined.inputProcessors
 local documentation = moduleBeingDefined.documentation
